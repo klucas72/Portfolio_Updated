1dedf8ae2222419dc6a994b6c64304627a6a62d7
klucas72 <klucas72@hotmail.com>
2021-07-06 16:46:41 -0400

    fixed classes on contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,38 +5,38 @@ export default function contact() {
     return (
 
         <div id="contact_info" class="col-md-10 col-sm-12">
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Phone</div>
-                <div class="card-body">
-                    <p class="card-text">(404) 550-5082</p>
+            <div nameClass="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div nameClass="card-header">Phone</div>
+                <div nameClass="card-body">
+                    <p nameClass="card-text">(404) 550-5082</p>
                 </div>
             </div>
 
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">email</div>
-                <div class="card-body">
-                    <p class="card-text">klucas72@hotmail.com</p>
+            <div nameClass="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div nameClass="card-header">email</div>
+                <div nameClass="card-body">
+                    <p nameClass="card-text">klucas72@hotmail.com</p>
                 </div>
             </div>
 
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">LinkedIn Profile</div>
-                <div class="card-body">
+            <div nameClass="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div nameClass="card-header">LinkedIn Profile</div>
+                <div nameClass="card-body">
                     <a href="https://www.linkedin.com/in/kristopher-lucas-1467355/" class="btn btn-dark">LinkedIn
                         profile</a>
                 </div>
             </div>
 
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Resume</div>
-                <div class="card-body">
+            <div nameClass="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div nameClass="card-header">Resume</div>
+                <div nameClass="card-body">
                     <a href="./assets/RESUME.pdf" class="btn btn-dark">Resume
                         Link</a>
                 </div>
             </div>
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">GitHub Profile</div>
-                <div class="card-body">
+            <div nameClass="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                <div nameClass="card-header">GitHub Profile</div>
+                <div nameclass="card-body">
                     <a href="https://github.com/klucas72" class="btn btn-dark">GitHub Profile Link</a>
                 </div>
             </div>
